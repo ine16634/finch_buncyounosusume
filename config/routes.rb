41ad682479de271b919482db61e69714f6a80716
ine16634 <62016478+ine16634@users.noreply.github.com>
2024-06-16 23:12:10 +0000

    トップページ作成3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'homes#top'
+  get '/home/about' => 'homes#about', as: "about"
+  get '/home/rule' => 'homes#rule', as: "rule"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit]
   resources :image_icons, only: [:new, :create, :index, :show, :destroy]

6c8f7585d100c2e3bda60515a4ffaf52e876618b
EC2 Default User <ec2-user@ip-172-31-14-178.ap-northeast-1.compute.internal>
2024-08-17 06:47:46 +0000

    実装フェーズ3完了6

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -10,7 +10,7 @@ class Answer < ApplicationRecord
   
   def get_image
     unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      file_path = Rails.root.join('app/assets/images/noimg.png')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
